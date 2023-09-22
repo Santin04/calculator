e161efef18ee0264a0839e961ee6ab5ff8fdf626
Santin04 <masantinte@gmail.com>
2023-09-22 09:11:44 -0300

    insert values in display where button active

--- scripit.js
+++ scripit.js
@@ -1,3 +1,5 @@
+//Inserindo e apagando dados:
+
 //criando a variavel que vai armazenar os dois campos da tela
 let tela01 = document.getElementById('res01')
 let tela02 = document.getElementById('res02')
@@ -7,7 +9,7 @@ function escrevendo(caracter){
 
     //vendo se é um caracter de operação
     if (caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/'){
-        tela01.innerHTML = `${tela02}${caracter}`
+        tela01.innerHTML = `${tela02.innerHTML}${caracter}`
         tela02.innerHTML = ``
     }
     else {
@@ -25,4 +27,18 @@ function apagar(){
     //escrevendo o valor com o último caracter apagado
     tela02.innerHTML = `${str}`
 
+}
+
+function apagarC(){
+    tela01.innerHTML = ``
+    tela02.innerHTML = ``
+}
+
+function apagarCE(){
+    tela02.innerHTML = ``
+}
+
+//Fazendo com que ao clicar na tecla insira tal número
+function inserir(num){
+    escrevendo(num)
 }
