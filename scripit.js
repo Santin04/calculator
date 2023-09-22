c5a55a2485c55d5c78eb53ec6806c68d04480d00
Santin04 <masantinte@gmail.com>
2023-09-22 13:00:42 -0300

    finish

--- scripit.js
+++ scripit.js
@@ -13,36 +13,40 @@ let divisao = false
 //escrevendo na tela
 function escrevendo(caracter){
 
-    //vendo se é um caracter de operação
-    if (caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/'){
-        tela01.innerHTML = `${tela02.innerHTML}${caracter}`
-        tela02.innerHTML = ``
-        
-        //guardando qual a operação:
-        if (caracter == '+'){
-            adicao = true
-            subtracao = false
-            multiplicacao = false
-            divisao = false
-        } else if (caracter == '-'){
-            adicao = false
-            subtracao = true
-            multiplicacao = false
-            divisao = false
-        } else if (caracter == '*'){
-            adicao = false
-            subtracao = false
-            multiplicacao = true
-            divisao = false
-        } else if (caracter == '/'){
-            adicao = false
-            subtracao = false
-            multiplicacao = false
-            divisao = true
-        }
+    if (tela02.innerHTML.length > 13){
+        window.alert('Número muito grande, tente fazer o cálculo com números menores')
+    } else {
+        //vendo se é um caracter de operação
+        if (caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/'){
+            tela01.innerHTML = `${tela02.innerHTML}${caracter}`
+            tela02.innerHTML = ``
+            
+            //guardando qual a operação:
+            if (caracter == '+'){
+                adicao = true
+                subtracao = false
+                multiplicacao = false
+                divisao = false
+            } else if (caracter == '-'){
+                adicao = false
+                subtracao = true
+                multiplicacao = false
+                divisao = false
+            } else if (caracter == '*'){
+                adicao = false
+                subtracao = false
+                multiplicacao = true
+                divisao = false
+            } else if (caracter == '/'){
+                adicao = false
+                subtracao = false
+                multiplicacao = false
+                divisao = true
+            }
 
-    }else {
-        tela02.innerHTML += `${caracter}`
+        }else {
+            tela02.innerHTML += `${caracter}`
+        }
     }
 }
 
@@ -74,40 +78,49 @@ function inserir(num){
     escrevendo(num)
 }
 
+valor01 = valor01.substring(0, valor01.length - 1)
+
+
 
 //Fazendo as operações matemáticas:
-function operacao(){
-    //pegandos os valores dos numeros que vao ser usados nas operações
-    let valor01 = tela01.innerHTML
+function operacao() {
+    let valor01 = tela01.innerHTML;
     valor01 = valor01.substring(0, valor01.length - 1)
-    valor01 = Number(valor01)
-    let valor02 = Number(tela02.innerHTML)
+    valor01 = parseFloat(valor01.replace(',', '.'));
+    
+    let valor02 = parseFloat(tela02.innerHTML.replace(',', '.'));
 
-    if (valor01 == ''){
-        valor01 = 0
+    if (isNaN(valor01)) {
+        valor01 = 0;
     }
 
-    if (valor02 == ''){
-        valor02 = 0
+    if (isNaN(valor02)) {
+        valor02 = 0;
     }
 
-    let res_final = 0 
+    let res_final = 0;
 
-    if (adicao == true){
-        res_final = mais(valor01, valor02)
-    } else if (subtracao == true){
-        res_final = menos(valor01, valor02)
-    } else if (multiplicacao == true){
-        res_final = vezes(valor01, valor02)
-    } else if (divisao == true){
-        res_final = dividi(valor01, valor02)
+    if (adicao) {
+        res_final = mais(valor01, valor02);
+    } else if (subtracao) {
+        res_final = menos(valor01, valor02);
+    } else if (multiplicacao) {
+        res_final = vezes(valor01, valor02);
+    } else if (divisao) {
+        res_final = dividi(valor01, valor02);
     }
 
-    console.log(res_final)
-
-    tela01.innerHTML = ``
-    tela02.innerHTML = `${res_final}`
+    res_final = res_final.toFixed(2);
+    
+    if (res_final > 13){
+        window.alert('O valor da conta é muito alto, tente com números menores')
+    } else {
+        res_final = res_final.toString().replace('.', ',')
+        tela01.innerHTML = '';
+        tela02.innerHTML = `${res_final}`
+    }
 }
+
 
 function mais(num1, num2){
     let res = num1 + num2
